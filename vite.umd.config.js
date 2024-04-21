ab8561f2ff69982c176fbed8809278ecde780608
wuzheng <1657955621@qq.com>
2024-04-21 14:31:20 +0800

    🐳 chore(打包): 添加 Vite 构建时相关的配置文件
    1. 添加构建为es模块和umd模块的对应配置文件；2. 修改package.json文件

---
+++ vite.umd.config.js
@@ -0,0 +1,39 @@
+/* 打包 umd 模块所用的配置文件 */
+import { fileURLToPath, URL } from 'node:url'
+import { resolve, dirname } from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  publicDir: false,
+  build: {
+    outDir: 'dist/umd', // 指定输出路径
+    lib: {
+      entry: resolve(__dirname, 'src/bundle.js'), // 打包的入口文件
+      name: 'ZhengUi',
+      fileName: 'zheng-ui', // 包文件输出的名称
+      formats: ['umd'] // 输出包格式 指定为 umd
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
